4e115b478e4513014796767dfdd138a0c195474d
iyasmzn <iyasmzn07@gmail.com>
2020-08-10 10:31:33 +0700

    check 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Iyasmzn
                 </div>
 
                 <div class="links">
