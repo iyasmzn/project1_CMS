a103004ae39c9993e3fd448f23dc178b4e199e36
iyasmzn <iyasmzn07@gmail.com>
2020-08-10 11:01:07 +0700

    check 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Iyasmzn07
+                    Iyasmzn07@
                 </div>
 
                 <div class="links">
